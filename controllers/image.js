c4fe059fbc38363e68493cb7cc18bffa8c472de4
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-08-01 14:16:29 +0200

    fixing security issues

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '2deefc546d74474e8a28f4ecb3b18472'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+            console.log(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'));
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage,
+    handleApiCall
 }
